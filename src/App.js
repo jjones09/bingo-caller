06c00b233a283ca82eb65b7f07ac77c82d637c29
Jordan Jones <jjones09@live.co.ukl>
2020-05-06 20:33:51 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, Grid } from '@material-ui/core';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [ calledNumbers, setCalledNumbers ] = useState([])
+
+  const callNextNumber = () => {
+    const nextNumber = Math.round((Math.random() * 90));
+    if (!!nextNumber && !calledNumbers.includes(nextNumber)) return nextNumber;
+    return callNextNumber();
+  }
+
+  const handleNextNumberClick = () => {
+    setCalledNumbers([...calledNumbers, callNextNumber()]);
+  }
+
+  const buildGrid = () => {
+    console.log(calledNumbers)
+    const gridRows = []
+    for (let y = 0; y < 9; y++) {
+      const gridColumn = []
+      for (let x = 1; x <= 10; x++) {
+        const num = 10 * y + x
+        const classStyle = calledNumbers.includes(num) ? 'called-number' : null;
+        gridColumn.push(
+          <Grid item key={`bingo-number-${num}`} className={`bingo-number ${classStyle}`}>{num}</Grid>
+        );
+      }
+      gridRows.push(
+        <div className='grid-row-wrapper'>
+          <Grid container justify='space-between' spacing={2}>{gridColumn}</Grid>
+        </div>
+      )
+    }
+    return gridRows;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Bingo
       </header>
+      <div className='app-contents'>
+        <div className='announcer-area'>
+          {calledNumbers.length ? <div>Last called number: {calledNumbers[calledNumbers.length - 1]}</div> : null}
+        </div>
+        <div className='card-container'>
+          {buildGrid()}
+        </div>
+        {
+          calledNumbers.length !== 90 ?
+            <Button variant="contained" color="link" onClick={handleNextNumberClick}>Next number</Button> :
+            null
+        }
+      </div>
     </div>
   );
 }
